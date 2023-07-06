9de8c67f70cbecd6179ef034d4e905564ef46480
jhoel debray quispe ticona <jhoel.quispe@ficct.uagrm.edu.bo>
2023-07-06 02:08:14 -0400

    dia del juicio f

--- database/migrations/2023_07_06_015938_tipo_pago.php
+++ database/migrations/2023_07_06_015938_tipo_pago.php
@@ -12,8 +12,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Tipo_pago::create(['tipo' => 'Efectivo']);
-        Tipo_pago::create(['tipo' => 'Tarjeta']);
+        Tipo_pago::create(['nombre' => 'Efectivo']);
+        Tipo_pago::create(['nombre' => 'Tarjeta']);
     }
 
     /**
