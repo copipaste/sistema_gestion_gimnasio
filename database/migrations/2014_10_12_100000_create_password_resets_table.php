4f4d0bb0025d8484c90f1c7eaab9eb9663990a2b
jhoel debray quispe ticona <jhoel.quispe@ficct.uagrm.edu.bo>
2023-07-11 22:07:12 -0400

    cambio z

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('password_resets', function (Blueprint $table) {
             $table->id();
-            $table->string('email')->index();
+            $table->string('email');
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
