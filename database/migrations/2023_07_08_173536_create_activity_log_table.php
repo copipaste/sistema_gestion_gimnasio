7086f02bc328c23317cb24e2da3cd1e7cb9ecd41
jhoel debray quispe ticona <jhoel.quispe@ficct.uagrm.edu.bo>
2023-07-16 02:06:32 -0400

    cambio reasasdasfadsasdasda

--- database/migrations/2023_07_08_173536_create_activity_log_table.php
+++ database/migrations/2023_07_08_173536_create_activity_log_table.php
@@ -8,7 +8,8 @@ class CreateActivityLogTable extends Migration
 {
     public function up()
     {
-        Schema::connection(config('activitylog.database_connection'))->create(config('activitylog.table_name'), function (Blueprint $table) {
+        Schema::create('activity_log', function (Blueprint $table) {
+        // Schema::connection(config('activitylog.database_connection'))->create(config('activitylog.table_name'), function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('log_name')->nullable();
             $table->text('description');
@@ -22,6 +23,7 @@ class CreateActivityLogTable extends Migration
 
     public function down()
     {
-        Schema::connection(config('activitylog.database_connection'))->dropIfExists(config('activitylog.table_name'));
+        // Schema::connection(config('activitylog.database_connection'))->dropIfExists(config('activitylog.table_name'));
+        Schema::dropIfExists('activity_log');
     }
 }
